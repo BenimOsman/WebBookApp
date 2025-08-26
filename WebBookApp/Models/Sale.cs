498911f1e6102140296b08b9191626cd2af76a5c
SYED <hello.fsyedrx@ibm.com>
2025-08-26 23:38:48 +0530

    Add project files.

---
+++ WebBookApp/Models/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBookApp.Models
+{
+    public class Sale
+    {
+        [Key]
+        public int SaleId { get; set; }
+        public int BookId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public virtual Book? Book { get; set; }
+        public virtual Customer? Customer { get; set; }
+    }
+}
